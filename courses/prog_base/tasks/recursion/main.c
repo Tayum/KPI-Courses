cffe770eaf6f2aaa4fdbd98d171a36cefc3f3db2
Maxim Gonchar <maxgonchar7@gmail.com>
2015-12-13 19:15:29 +0200

    Fix "Recursion" task

--- courses/prog_base/tasks/recursion/main.c
+++ courses/prog_base/tasks/recursion/main.c
@@ -4,26 +4,8 @@
 #define ERROR '0'
 #define ARRLEN 100
 
-char repSymbol(char *str, size_t len, char ch) {
-	int i;
-	//If it cannot be repeatable symbols
-	if (1 == len) {
-		return ERROR;
-	}
-	//Go through the str.
-	//If [0] and some symbol are equal - return [0].
-	for (i = 1; i < len; i++) {
-		if (str[0] == str[i]) {
-			ch = str[0];
-			return ch;
-		}
-	}
-	//If in str are no symbols like [0] - do function again,
-	//starting from the next symbol.
-	if (ch == ERROR) {
-		repSymbol(str + 1, len - 1, ch);
-	}
-}
+char checkIfCharContainsInArrElseReturnERROR(char *, size_t, char);
+char repSymbol(char *, size_t, char);
 
 int main(void) {
 
@@ -35,8 +17,38 @@ int main(void) {
 
 	//Find repeatable symbol
 	char resultCh = repSymbol(buff, len, ERROR);
+	//char resultCh = checkIfCharContainsInArrElseReturnERROR(buff, len, 'w');
 	printf("%c\n", resultCh);
 
 	getchar();
 	return 0;
+}
+
+//I don`t like recursion functions
+char checkIfCharContainsInArrElseReturnERROR(char *arr, size_t len, char ch) {
+	if (0 == len) {
+		return ERROR;
+	}
+	//If something is similar
+	else if (ch == arr[0]) {
+		return ch;
+	}
+	//If something is not similar
+	else {
+		checkIfCharContainsInArrElseReturnERROR(arr + 1, len - 1, ch);
+	}
+}
+
+char repSymbol(char *str, size_t len, char ch) {
+	//If it cannot be repeatable symbols
+	if (1 == len) {
+		return ERROR;
+	}
+	//Go through the str and find if str[0] repeats in this array.
+	ch = checkIfCharContainsInArrElseReturnERROR(str + 1, len, str[0]);
+	//If in str are no symbols like [0] - do function again,
+	//starting from the next symbol.
+	if (ch == ERROR) {
+		repSymbol(str + 1, len - 1, ch);
+	}
 }
