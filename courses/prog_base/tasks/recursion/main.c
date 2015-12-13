79b5ab9a6bad4b6cd92b3fd9b178bbd12084fe4b
Maxim Gonchar <maxgonchar7@gmail.com>
2015-12-13 22:49:42 +0200

    Fix recursion

--- courses/prog_base/tasks/recursion/main.c
+++ courses/prog_base/tasks/recursion/main.c
@@ -4,8 +4,7 @@
 #define ERROR '0'
 #define ARRLEN 100
 
-char checkIfCharContainsInArrElseReturnERROR(char *, size_t, char);
-char repSymbol(char *, size_t, char);
+char repSymbol(char *, size_t);
 
 int main(void) {
 
@@ -16,39 +15,21 @@ int main(void) {
 	int len = strlen(buff);
 
 	//Find repeatable symbol
-	char resultCh = repSymbol(buff, len, ERROR);
-	//char resultCh = checkIfCharContainsInArrElseReturnERROR(buff, len, 'w');
+	char resultCh = repSymbol(buff, len);
 	printf("%c\n", resultCh);
 
 	getchar();
 	return 0;
 }
 
-//I don`t like recursion functions
-char checkIfCharContainsInArrElseReturnERROR(char *arr, size_t len, char ch) {
-	if (0 == len) {
-		return ERROR;
-	}
-	//If something is similar
-	else if (ch == arr[0]) {
-		return ch;
-	}
-	//If something is not similar
-	else {
-		checkIfCharContainsInArrElseReturnERROR(arr + 1, len - 1, ch);
-	}
-}
-
-char repSymbol(char *str, size_t len, char ch) {
-	//If it cannot be repeatable symbols
+char repSymbol(char *str, size_t len) {
 	if (1 == len) {
 		return ERROR;
 	}
-	//Go through the str and find if str[0] repeats in this array.
-	ch = checkIfCharContainsInArrElseReturnERROR(str + 1, len, str[0]);
-	//If in str are no symbols like [0] - do function again,
-	//starting from the next symbol.
-	if (ch == ERROR) {
-		repSymbol(str + 1, len - 1, ch);
+	else if (str[len] == str[len-1]) {
+		return str[len];
+	}
+	else {
+		repSymbol(str, len - 1);
 	}
 }
