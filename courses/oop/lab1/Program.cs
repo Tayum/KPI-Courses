a3df6e24d1e698da82af3a419c9291a81dd86989
Maxim Gonchar <maxgonchar9@gmail.com>
2016-09-26 01:16:24 +0300

    Main class

---
+++ courses/oop/lab1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OOP_Lab1_Gonchar_Maxim_KP51
+{
+    class GameMain
+    {
+        static void Main(string[] args)
+        {
+            Model a = new Model(123, 5, 5);
+            Console.WriteLine(a);
+        }
+    }
+}
